55c8cd750be091dec38dfd18411199868b7887b3
koldburn <68352536+koldburn@users.noreply.github.com>
2020-09-16 20:18:35 +0000

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 def call(Map config = [:]){
-    echo "hello ${config.name}, from ${config.city}, this is $[config.day}"
+    echo "hello ${config.name}, from ${config.city} this is $[config.day}"
 }
